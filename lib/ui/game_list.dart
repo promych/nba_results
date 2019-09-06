bf30333f4b878a1c55b5fcdccbd8b08af75fb5a4
Roman <thetrusty@gmail.com>
2019-09-06 22:17:32 +0300

    Add list animation

--- lib/ui/game_list.dart
+++ lib/ui/game_list.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+
 import '../models/game.dart';
 import '../ui/game_card.dart';
 
 class GameList extends StatelessWidget {
   final List<Game> games;
 
-  const GameList({Key key, @required this.games});
+  const GameList({@required this.games});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      scrollDirection: Axis.vertical,
-      padding: EdgeInsets.all(10.0),
-      itemCount: games.length,
-      itemBuilder: (BuildContext context, int index) {
-        return GameCard(game: games[index]);
-      },
+    return AnimationLimiter(
+      child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: games.length,
+        itemBuilder: (BuildContext context, int index) {
+          return AnimationConfiguration.staggeredList(
+            position: index,
+            duration: const Duration(milliseconds: 375),
+            child: SlideAnimation(
+              verticalOffset: 50.0,
+              child: FlipAnimation(
+                child: GameCard(game: games[index]),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
